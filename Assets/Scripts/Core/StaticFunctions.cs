089e4920db21f9f910a1935a022161d3a1c88a56
ZircliX <lolofy71@gmail.com>
2024-11-27 10:30:43 +0100

    Partial Metrics + StaticFunctions

--- Assets/Scripts/Core/StaticFunctions.cs
+++ Assets/Scripts/Core/StaticFunctions.cs
@@ -7,48 +7,6 @@ namespace CIRC.Core
 {
     public static partial class StaticFunctions
     {
-        public static Vector2 GetRandomPositionWithinRectTransform(this RectTransform imageRectTransform)
-        {
-            float randomX = Random.value;
-            float randomY = Random.value;
-            
-            float imageWidth = imageRectTransform.rect.width;
-            float imageHeight = imageRectTransform.rect.height;
-
-            Vector2 localPosition = new Vector2(randomX * imageWidth - imageWidth / 2, randomY * imageHeight - imageHeight / 2);
-            return localPosition;
-        }
-
-        public static Vector3 FromScreenPointToWorldPoint(this Vector3 screenPosition)
-        {
-            if (Camera.main)
-            {
-                Vector3 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
-                worldPosition = new Vector3(worldPosition.x, worldPosition.y, 0f);
-                return worldPosition;
-            }
-            
-            return Vector3.zero;
-        }
-        
-        public static Bounds RectTransformToScreenSpace(this RectTransform rectTransform)
-        {
-            Vector3[] corners = new Vector3[4];
-            rectTransform.GetWorldCorners(corners);
-
-            Vector3 position = (corners[0] + corners[2]) / 2;
-            Vector3 size = corners[2] - corners[0];
-
-            return new Bounds(position, size);
-        }
-
-        private static readonly Vector3[] corners = new Vector3[4];
-        public static Bounds RectTransformToWorldBounds(this RectTransform rectTransform)
-        {
-            rectTransform.GetWorldCorners(corners);
-            return GeometryUtility.CalculateBounds(corners, Matrix4x4.identity);
-        }
-        
         public static IEnumerator TakeScreenshotAndShare(this bool hasScreen)
         {
             yield return new WaitForEndOfFrame();
