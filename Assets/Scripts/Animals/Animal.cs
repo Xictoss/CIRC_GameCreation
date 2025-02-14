d1c5bfe0c8ba4a65f07c3613c942313313fa3dc3
ZircliX <lolofy71@gmail.com>
2025-02-14 09:57:21 +0100

    Dialogue System

---
+++ Assets/Scripts/Animals/Animal.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using UnityEngine;
+
+namespace CIRC.Animals
+{
+    public class Animal : MonoBehaviour
+    {
+        [field : SerializeField] public AnimalData AnimalData { get; private set; }
+
+        private IEnumerator Start()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(AnimalData.dialogCooldown);
+                
+                DialogsManager.Instance.SpawnDialog(AnimalData, transform.position);
+            }
+        }
+    }
+}
