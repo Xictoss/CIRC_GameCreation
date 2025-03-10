ece39ed396bd70e5c873736fbdfdf6418869f0eb
Dydou <MegaFight63@laposte.net>
2025-03-10 16:02:53 +0100

    MiniGame HydratationVilleTaper Finito

---
+++ Assets/Scripts/MiniGames/Sample/6_Ville/HydratationVilleTaper/HydratationVilleTaperContext.cs
@@ -0,0 +1,12 @@
+
+using CIRC.MiniGames.Core.Interfaces;
+using CIRC.Progression;
+
+namespace CIRC.MiniGames.Sample
+{
+    public struct HydratationVilleTaperContext : IMiniGameContext
+    {
+        public MiniGameDataHolder miniGameData;
+        public bool isRight;
+    }
+}
